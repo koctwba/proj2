772fde54a84ed2145b559c18502058db1417b4ed
koctwba <76501232+koctwba@users.noreply.github.com>
2021-01-04 23:56:38 +0900

    1.4_23:56

--- js/slide.js
+++ js/slide.js
@@ -5,6 +5,14 @@ $(document).ready(function(){
             default_page      : '1',                     // Default/ first page to load
             animation         : 'glueTopBottom'
         });
+        $(".lazy").slick({
+            lazyLoad: 'ondemand', // ondemand progressive anticipated
+            infinite: true,
+            dots : true,
+            autoplay : true,
+            autoplaySpeed : 10000,
+            pauseOnHover : true 
+          });
     
     });
 
