53262532368daf1acee9001529b6ab12f1b61291
koctwba <76501232+koctwba@users.noreply.github.com>
2021-01-04 12:07:43 +0900

    1.4_12:07

---
+++ js/slide.js
@@ -0,0 +1,8 @@
+$(function(){
+    $('#pages').wankyPages({
+          selector_prefix   : 'wanky_',
+          default_page      : '1',
+          animation         : 'glueTopBottom'
+        });
+        
+})
