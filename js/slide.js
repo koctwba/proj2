100155700a22b2db2d24b4e94a19d4a33bffcf05
koctwba <76501232+koctwba@users.noreply.github.com>
2021-01-04 20:07:33 +0900

    1.4_20:07

--- js/slide.js
+++ js/slide.js
@@ -1,8 +1,12 @@
-$(function(){
-    $('#pages').wankyPages({
-          selector_prefix   : 'wanky_',
-          default_page      : '1',
-          animation         : 'glueTopBottom'
+$(document).ready(function(){
+
+        $('#pages').wankyPages({
+            selector_prefix   : 'wanky_',                // Prefix to give all classes and ID's (apart from animations)
+            default_page      : '1',                     // Default/ first page to load
+            animation         : 'glueTopBottom'
         });
-        
-})
+    
+    });
+
+
+  
