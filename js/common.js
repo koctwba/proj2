d2a9be72c1f2621b034475984a4148243283e34c
koctwba <76501232+koctwba@users.noreply.github.com>
2021-01-06 15:07:47 +0900

    1.6_15:07

--- js/common.js
+++ js/common.js
@@ -2,7 +2,6 @@ $(function(){
     $('.top').click(function(){
         $('body,html').animate({scrollTop:0},1000);
     })
-
     var sta = 1;
     $('.search').click(function(){
        if (sta == 1) {
@@ -14,8 +13,19 @@ $(function(){
         sta = 1;    
     }
    )}
-}) 
-    
-     
-    
 })
+    $('.aalto').hover(function(){
+        $('.aalto img').css({'opacity':'0.5'});
+        $('.aalto img').css({'transform':'scale(1.1)'});
+        $('.aalto p').css({'color':'#95340A'});
+        $('.aalto span::after').css({'color':'#999999'});
+        $('.aalto .view_more').css({'color':'#A38C7E'});
+    },function(){
+        $('.aalto img').css({'opacity':'1'});
+        $('.aalto img').css({'transform':'scale(1)'});
+        $('.aalto p').css({'color':'#5D2D10'});
+        $('.aalto span::after').css({'color':'#88451E'});
+        $('.aalto .view_more').css({'color':'#A38C7E'});
+    })
+
+})//jq end
